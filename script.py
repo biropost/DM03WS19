e06ca6b447023e711bebb4ee3160f5f50010fa88
Bernhard Frick <bernhard.frick@gmx.at>
2019-12-10 01:04:24 +0100

    add more output

--- script.py
+++ script.py
@@ -5,8 +5,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from scipy.io import arff
+from sklearn import metrics
 
 from predecon import PreDeCon
+
+print("Loading Dataset...")
 
 fName = 'data/iris.arff'
 tData, meta = arff.loadarff(fName)
@@ -15,46 +18,39 @@ D = pd.DataFrame(data=tData)
 true_labels = D["class"]
 D = D.drop("class", axis=1)
 
-# 1. Run PreDeCon
+print("Clustering Data using PreDeCon...")
 
 start = time.time()
+
 pdc = PreDeCon(e=0.5, m=1, l=2, d=0.05)
 clustered_labels = pdc.fit(D)
+
 end = time.time()
-print("Runtime:", end - start, "seconds")
-
-# 2. Flatten the clustered labels (contains nested arrays with 1 element each, possibly fix in PreDeCon.fit())
-
-clustered_labels = list(itertools.chain.from_iterable(clustered_labels))
-
-# 3. Get all unique labels
+print(" |- Runtime:", end - start, "seconds")
 
 unique_labels = np.unique(clustered_labels)
-print("Number of Clusters:", len(unique_labels))
+print(" |- Number of Clusters:", len(unique_labels))
 
-# 4. Assign a random color to each unique cluster id
+print("Preparing Data for Plots...")
 
-# color_map is a dict: label name -> random color
+# Flatten the clustered labels (contains nested arrays with 1 element each, possibly fix in PreDeCon.fit())
+clustered_labels = list(itertools.chain.from_iterable(clustered_labels))
+
+# Assign a random color to each unique cluster id
 color_map = dict(zip(unique_labels, np.random.rand(len(unique_labels), )))
 
-# 5. map clustered_labels to colors list
-
+# map clustered_labels to colors list
 colors = list(map(color_map.get, clustered_labels))
 
-# 6. Plot all subspaces with colored points
+print("Generating Subspace Plots...")
 
 names = meta.names()
 names.remove("class")
-
 plt_index = 1
-
-# now add colors based on clustered_labels
-
 for i, name in enumerate(names):
     for j in range(i+1, len(names)):
         x = D[names[i]]
         y = D[names[j]]
-        # color = np.sqrt(x ** 2 + y ** 2)
         plt.subplot(3, 2, plt_index)
         plt_index = plt_index + 1
         plt.scatter(x, y, s=30, c=colors, marker="+")
